09f00d5d4390f1ea27228b03a134dd1053762048
Miguel Colmenares <expertnestco@gmail.com>
2025-10-21 21:05:05 -0700

    updated

--- components/ClientLayout.tsx
+++ components/ClientLayout.tsx
@@ -106,7 +106,7 @@ export default function ClientLayout({ children }: { children: ReactNode }) {
 
   return (
     <MusicProvider>
-      <div className="relative w-full md:h-screen flex-1 flex flex-col overflow-hidden">
+      <div className="relative w-full h-screen flex-1 flex flex-col overflow-hidden">
         <div className="flex flex-1 flex-col overflow-hidden">
           {/* Desktop Navbar */}
           {!hideSidebars && (
